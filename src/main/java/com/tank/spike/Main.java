01130b5a212e6d5ca050c00cc92163f28da5cf3f
fuchun198435163.com <tank198435@163.com>
2020-11-19 09:13:27 +0800

    basic framework

---
+++ src/main/java/com/tank/spike/Main.java
@@ -0,0 +1,10 @@
+package com.tank.spike;
+
+/**
+ * @author tank198435163.com
+ */
+public class Main {
+  public static void main(final String[] args) {
+    System.out.println("Hello World!");
+  }
+}
