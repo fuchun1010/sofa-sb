7c4165d0cad1c34e98d00a3bb81396672c06c1c6
fuchun198435163.com <tank198435@163.com>
2020-11-19 09:36:16 +0800

    [fuchun] #doc basic run success

--- src/main/java/com/tank/spike/Main.java
+++ src/main/java/com/tank/spike/Main.java
@@ -1,10 +1,15 @@
 package com.tank.spike;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+
 /**
  * @author tank198435163.com
  */
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class Main {
   public static void main(final String[] args) {
-    System.out.println("Hello World!");
+    SpringApplication.run(Main.class);
   }
 }
