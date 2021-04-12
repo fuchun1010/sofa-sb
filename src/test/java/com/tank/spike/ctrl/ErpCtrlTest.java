5d7e0036912a5679724c779df5485eca7ff46842
fuchun198435163.com <tank198435@163.com>
2021-04-12 11:33:50 +0800

    [fuchun] #tech mock controller

---
+++ src/test/java/com/tank/spike/ctrl/ErpCtrlTest.java
@@ -0,0 +1,51 @@
+package com.tank.spike.ctrl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tank.spike.TestBase;
+import com.tank.spike.constants.UrlPrefix;
+import com.tank.spike.protocol.resp.ResultApi;
+import lombok.SneakyThrows;
+import lombok.val;
+import org.junit.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.util.AssertionErrors;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import javax.annotation.Resource;
+
+import static com.tank.spike.constants.UrlPrefix.URL_ACTIVE_MEMBERS;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @author tank198435163.com
+ */
+@AutoConfigureMockMvc
+public class ErpCtrlTest extends TestBase {
+
+  @Test
+  @SneakyThrows
+  public void obtainActiveMemberTest() {
+    AssertionErrors.assertNotNull("objectMapper not allowed empty", objectMapper);
+    val requestBuilder = MockMvcRequestBuilders.get(UrlPrefix.URL_PREFIX + URL_ACTIVE_MEMBERS);
+    val mvcResult = this.mockMvc.perform(requestBuilder.accept(MediaType.APPLICATION_JSON))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.data").exists())
+            .andReturn();
+
+    AssertionErrors.assertNotNull("mvnResult Not allowed empty", mvcResult);
+    val body = mvcResult.getResponse().getContentAsByteArray();
+    val activeMemberResp = this.objectMapper.readValue(new String(body), ResultApi.class);
+    AssertionErrors.assertNotNull("activeMemberResp not allowed empty", activeMemberResp);
+  }
+
+  @Resource
+  private MockMvc mockMvc;
+
+  @Resource
+  private ObjectMapper objectMapper;
+}
