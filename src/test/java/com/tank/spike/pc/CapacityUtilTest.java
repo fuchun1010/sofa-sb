a68276c8fcf7ec7b15e0825ee27857754de0e0a1
fuchun198435163.com <tank198435@163.com>
2021-05-25 08:52:43 +0800

    [fuchun] #refactor some math calculate

--- src/test/java/com/tank/spike/pc/CapacityUtilTest.java
+++ src/test/java/com/tank/spike/pc/CapacityUtilTest.java
@@ -5,6 +5,8 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.test.annotation.Repeat;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class CapacityUtilTest {
@@ -29,5 +31,35 @@ public class CapacityUtilTest {
     Assert.assertTrue(result > 1);
   }
 
-  private ThreadLocalRandom random = ThreadLocalRandom.current();
+  @Test
+  public void testRemaining() {
+    val appleUnitPrice = 30;
+    val appleAmount = 5;
+    val pearUnitPrice = 20;
+    val pearAmount = 6;
+
+    val pearRawPrice = new BigDecimal(pearUnitPrice * pearAmount);
+    val appleRawPrice = new BigDecimal(appleUnitPrice * appleAmount);
+    val amount = appleRawPrice.add(pearRawPrice);
+
+    Assert.assertEquals(amount.intValue(), 270);
+
+    val discountForOrder = new BigDecimal(80);
+    val lastPay = amount.subtract(new BigDecimal(80));
+    Assert.assertEquals(lastPay.intValue(), 190);
+
+    val appleRation = appleRawPrice.multiply(discountForOrder).divide(amount, 2, RoundingMode.FLOOR);
+    val pearRation = pearRawPrice.multiply(discountForOrder).divide(amount, 2, RoundingMode.FLOOR);
+
+    val remaining = discountForOrder.subtract(appleRation).subtract(pearRation);
+
+    val applePay = appleRawPrice.subtract(appleRation);
+    val pearPay = pearRawPrice.subtract(pearRation);
+    val result = applePay.add(pearPay).add(remaining).intValue();
+    Assert.assertEquals(lastPay.intValue(), result);
+    System.out.println("xxx");
+  }
+
+
+  private static final ThreadLocalRandom random = ThreadLocalRandom.current();
 }
